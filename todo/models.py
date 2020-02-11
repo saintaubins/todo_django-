92a42717681f54c869ec71e1c2bc7395d82729ec
Danny Gleason <danny.c.gleason@gmail.com>
2020-02-11 16:24:24 -0500

    changed model, added todos

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from datetime import datetime
+
 class Todos(models.Model):
-    author = models.CharField(max_length=200)
-    nationality = models.CharField(max_length=100)
-    title = models.CharField(max_length=500)
+    text = models.CharField(max_length=100, default='default')
+    when = models.CharField(max_length=500, default='default')
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
-        return self.author + " , " + self.nationality + " , " + self.title
+        return self.text
 
